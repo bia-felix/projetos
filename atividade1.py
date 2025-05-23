b4ab2576f24d517c6813790ce13e0b25953bc883
bia <felixbia.lua@gmail.com>
2025-05-23 16:57:00 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,4 +1,8 @@
 nome =input ('digite seu nome:')
 idade =int(input ('digite sua idade:'))
 ano =int(input ('digite seu ano escolar:'))
-print(nome, idade, ano)
+print('seu nome é:',nome )
+
+print('sua idade é:',idade)
+
+print('seu ano escolar é:',ano)
