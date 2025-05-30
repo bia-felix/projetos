2b09844667f14a828832c485286bb9ee76ea3a4c
bia <felixbia.lua@gmail.com>
2025-05-30 15:51:44 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+login=input("digite o login:")
+senha=input("digite a senha:")
+
+if login == "login" and senha == "senha":
+    print("senha confirmada!!")
+    print("login confirmada!!")
+else:
+    print("revisar !!!")
