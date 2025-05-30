44bc994e7445b2505b6e6929af7cd4ff2dfd9b89
bia <felixbia.lua@gmail.com>
2025-05-30 16:00:28 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,12 @@
+nome=input("digite o nome do aluno:")
+nota1=float(input("digite a primeira nota:"))
+nota2=float(input("digite a segunda nota:"))
+media=(nota1+nota2)/2
+print("nome do aluno:",nome)
+print("primeira nota é:",nota1)
+print("primeira nota é:",nota2)
+print("a media é:",media)
+if media >7:
+    print("aluno foi aprovado")
+else:
+    print("O aluno foi reprovado")
