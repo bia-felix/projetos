e03b590fdeec18d2dfd6bcb80aa46eb48f376cc3
bia <felixbia.lua@gmail.com>
2025-06-27 16:39:51 -0300

    Create asjfhlaf.py

---
+++ asjfhlaf.py
@@ -0,0 +1,21 @@
+#e verificar a situação do aluno (aprovado ou reprovado, usando condicionais).
+
+#alunos = {"nome" : {"matricula": 123445, "notas": []}}
+
+def adicinar alunos():
+    nome1= bia
+    matricula1=5222
+    nome2=diego
+    matricula2=5543658456
+    nome3= dama
+    matricula3=2116475564
+    e matricula
+    
+def adicinae notas()
+    aluno ja exitww e nota
+    
+def calcular media()
+
+def exibir alinos()
+    tudo
+
