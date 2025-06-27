e61a7cf0f18569c9ff8064504951f4c78fc60ed7
bia <felixbia.lua@gmail.com>
2025-06-27 15:33:49 -0300

    Create livros.py

---
+++ livros.py
@@ -0,0 +1,8 @@
+livros={
+"titulo": "O Pequeno Príncipe",
+"autor": "Antoine de Saint-Exupéry",
+"ano_publicacao": 1943
+}
+livros["genero"]="fabula"
+print(livros)
+print(f"autor:{livros['autor']}") 
